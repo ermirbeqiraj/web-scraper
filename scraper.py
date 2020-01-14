519d876f5743dbf9601db3f2cf54fe8b3a195cf3
Ermir Beqiraj <ermirbeqiraj@hotmail.com>
2020-01-14 15:04:03 +0100

    main file

---
+++ scraper.py
@@ -0,0 +1,10 @@
+from boards import indeed, monster
+
+#
+# monster jobs url to search for remote C#:
+# https://www.monster.com/jobs/search/?q=C__23&where=Remote
+#
+
+# indeed jobs url to search for remote C#:
+# https://www.indeed.com/jobs?as_and=C%23&jt=all&l=Remote&fromage=1
+#
